4bfd1b04d63302908d558ea84df2cd1bc26c3ebd
dingyling <simen_hel@hotmail.com>
2015-03-01 18:02:17 +0100

    asd

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,21 @@
 var init = function() {
+    $('.knapp').click(function() {
+        alert("I am an alert box!");
+    });
 
+    var map = L.map('leaflet-kart', {
+        maxZoom: 24,
+        minZoom: 1,
+        zoomControl: false,
+        crs: L.CRS.Simple
+    }).setView([0, 0], 1);
+
+    map.setMaxBounds(new L.LatLngBounds([0,500], [500,0]));
+
+    var imageUrl = 'http://i57.tinypic.com/2i8deac.png'
+    var imageBounds = [[500,0], [0,370]];
+
+    L.imageOverlay(imageUrl, imageBounds).addTo(map);
 };
 
     $(document).ready(init);
