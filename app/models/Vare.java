6911cfd89007b597ebdf3e1ee02e9c4e6d43b56b
annesofie <aserichs@stud.ntnu.no>
2015-03-03 13:06:00 +0100

    Update Vare.java

--- app/models/Vare.java
+++ app/models/Vare.java
@@ -4,4 +4,15 @@ package models;
  * Created by annesofiestranderichsen on 03.03.15.
  */
 public class Vare {
+  @SuppressWarnings("unused")
+	public Vare(int vareID){
+		DatabaseVarer db= new DatabaseVarer();
+		String navn= db.getVareNavn(vareID);
+		String pris= db.getPrice(vareID);
+		String kategori=db.getKategor(vareID);
+		String xValue = db.getPlaceX(vareID);
+		String yValue = db.getPlaceY(vareID);
+		String zValue =db.getPlaceZ(vareID);	
+	}
+  
 }
