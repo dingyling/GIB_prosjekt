23e5e4d5d6f02877da7cf97ec8637123f8f20184
annesofie <aserichs@stud.ntnu.no>
2015-03-03 13:05:23 +0100

    Update User.java

--- app/models/User.java
+++ app/models/User.java
@@ -3,5 +3,15 @@ package models;
 /**
  * Created by annesofiestranderichsen on 03.03.15.
  */
+ 
+import java.util.ArrayList;
+ 
 public class User {
+  DatabaseVarer db=new DatabaseVarer();
+	@SuppressWarnings("unused")
+	public User(String epost){
+		String passord=db.getPassword(epost);
+		ArrayList<String> handleliste = db.getHandleliste(epost);
+		
+	}
 }
